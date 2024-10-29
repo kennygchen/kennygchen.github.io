c91076461e0fb29ca4718bb2843ee4660b53afe6
Kenny Chen <kennygchen@yahoo.com>
2024-10-28 17:01:33 -0700

    nav bar

---
+++ app/ui/nav-links.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import clsx from 'clsx';
+
+const links = [
+  { name: 'Home', href: '/' },
+  { name: 'About', href: '/about' },
+  { name: 'Contact', href: '/contact' },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex space-x-6 text-lg font-medium">
+      {links.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+          className={clsx(
+            'hover:text-gray-300',
+            {
+              'text-[--color-secondary] border-b-2 border-[--color-secondary]': pathname === link.href,
+              'text-gray-400': pathname !== link.href,
+            }
+          )}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
